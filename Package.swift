7d1212ab3d9bc6a1447681bf7ebd0cb41472ecc7
Pavlo Boiko <boiko.impul@gmail.com>
2021-07-22 07:50:26 +0300

    update package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FusionBluetooth",
+    platforms: [.macOS(.v10_14), .iOS(.v12)],    
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,17 +13,41 @@ let package = Package(
             targets: ["FusionBluetooth"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Android", url: "https://github.com/scade-platform/swift-android.git", .branch("android/24"))        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FusionBluetooth",
-            dependencies: []),
+            dependencies: [
+              .target(name: "FusionBluetooth_Common"),              
+              .target(name: "FusionBluetooth_Apple", condition: .when(platforms: [.iOS, .macOS])),
+              .target(name: "FusionBluetooth_Android", condition: .when(platforms: [.android])),
+            ] 
+        ),
+        .target(
+            name: "FusionBluetooth_Common"
+        ),  
+        .target(
+            name: "FusionBluetooth_Apple",
+            dependencies: [
+              .target(name: "FusionBluetooth_Common"),
+            ]                        
+        ),
+        .target(
+            name: "FusionBluetooth_Android",
+            dependencies: [
+              .target(name: "FusionBluetooth_Common"),
+              .product(name: "Android", package: "Android", condition: .when(platforms: [.android])),
+              .product(name: "AndroidOS", package: "Android", condition: .when(platforms: [.android])),
+              .product(name: "AndroidLocation", package: "Android", condition: .when(platforms: [.android]))              
+            ],
+            resources: [.copy("Generated/FusionBluetooth_Android.java")]         
+        )
         .testTarget(
             name: "FusionBluetoothTests",
             dependencies: ["FusionBluetooth"]),
     ]
 )
+
