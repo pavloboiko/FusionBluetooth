0969806868374020a52c2548eab4fa0531b57c1d
Pavlo Boiko <boiko.impul@gmail.com>
2021-07-22 14:06:26 +0300

    fix error

--- Package.swift
+++ Package.swift
@@ -2,6 +2,9 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
+
+let SCADE_SDK = ProcessInfo.processInfo.environment["SCADE_SDK"] ?? ""
 
 let package = Package(
     name: "FusionBluetooth",
